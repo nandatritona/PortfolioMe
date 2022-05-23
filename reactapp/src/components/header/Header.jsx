7a102e34668a9e07a81605b9c91b3354cc8193eb
unknown <user@ITD35.ITD35>
2022-05-23 09:49:07 +0700

    Membuat Header

--- reactapp/src/components/header/Header.jsx
+++ reactapp/src/components/header/Header.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import './header.css'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header>
+            <div className="container header_container">
+                <h5>Hallo I'm</h5>
+                <h1>Nanda Tritona</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+            </div>
+        </header>
+    )
 }
 
 export default Header;
