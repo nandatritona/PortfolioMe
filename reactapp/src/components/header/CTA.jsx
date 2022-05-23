78cabd52e6c98d95fc176334182ccd648aa7c1b0
unknown <user@ITD35.ITD35>
2022-05-23 10:15:28 +0700

    menambahkan contoh cv

--- reactapp/src/components/header/CTA.jsx
+++ reactapp/src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/CV.pdf'
+import CV from '../../assets/cvme.pdf'
 
 export const CTA = () => {
     return (
--- reactapp/src/components/header/Header.jsx
+++ reactapp/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
 
 export const Header = () => {
     return (
@@ -8,6 +9,7 @@ export const Header = () => {
                 <h5>Hallo I'm</h5>
                 <h1>Nanda Tritona</h1>
                 <h5 className="text-light">Fullstack Developer</h5>
+                <CTA />
             </div>
         </header>
     )
