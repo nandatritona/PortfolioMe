679fa8a5071bd6e2b1e1ece95aace6bef30fd894
nandatritona <nandhatritona@gmail.com>
2022-05-23 22:37:48 +0700

    Desain about me

--- reactapp/src/components/about/About.jsx
+++ reactapp/src/components/about/About.jsx
@@ -9,45 +9,45 @@ import {VscFolderLibrary} from "react-icons/vsc";
 
 export const About = () => {
     return (
-        <section id='about'>
-            <h5>Get To Know</h5>
-            <h2>About Me</h2>
+      <section id="about">
+        <h5>Get To Know</h5>
+        <h2>About Me</h2>
 
-            <div className="container about_container">
-                <div className="about_me">
-                    <img src={ME} alt="About Image"/>
-                </div>
-
-                <div className="about_content">
-                    <div className="about_cards">
-                        <article className="about_cards">
-                            <FaAward className='about_icon'/>
-                            <h5>Experience</h5>
-                            <small>3* Years Working</small>
-                        </article>
-
-                        <article className="about_cards">
-                            <BsFillCreditCard2FrontFill className='about_icon'/>
-                            <h5>Sertifikat</h5>
-                            <small>200 Worldwide</small>
-                        </article>
-
-                        <article className="about_cards">
-                            <VscFolderLibrary className='about_icon'/>
-                            <h5>Project</h5>
-                            <small>10+ Complated</small>
-                        </article>
-                    </div>
-
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur est nisi minima incidunt consequatur molestias aut quisquam vitae vero quos ex, exercitationem impedit neque, itaque nam aperiam, velit commodi. Ad!
-                    </p>
-
-                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <div className="container about_container">
+            <div className="about_me">
+                <div className="about_me_image">
+                    <img src={ME} alt="About Image" />
                 </div>
             </div>
-        </section>
-    )
+        
+            <div className="about_content">
+                <div className="about_cards">
+                    <article className="about_card">
+                        <FaAward className="about_icon" />
+                        <h5>Experience</h5>
+                        <small>3* Years Working</small>
+                    </article>
+
+                    <article className="about_card">
+                        <BsFillCreditCard2FrontFill className="about_icon" />
+                        <h5>Sertifikat</h5>
+                        <small>200 Worldwide</small>
+                    </article>
+
+                    <article className="about_card">
+                        <VscFolderLibrary className="about_icon" />
+                        <h5>Project</h5>
+                        <small>10+ Complated</small>
+                    </article>
+                </div>
+
+                <p>"Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya."</p>
+
+                <a href="#contact" className="btn btn-primary">Let's Talk</a>
+            </div>
+        </div>
+      </section>
+    );
 }
 
 export default About;
